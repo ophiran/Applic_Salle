8be20b76d5418fff901d2c3ae56a89f6b8dd8d50
ekym <m.davidmike@gmail.com>
2013-04-18 11:03:39 +0200

    Fin news

--- src/Applic_Salle/News.java
+++ src/Applic_Salle/News.java
@@ -8,6 +8,25 @@ package Applic_Salle;
  *
  * @author ekimd_000
  */
+
 public class News {
     
+    public static enum TypeNews {
+        POLITIQUE, INTER, SPORT, PEOPLE
+    }
+    
+    private String contenu;
+    private Journaliste auteur;
+    private boolean importance;
+    private TypeNews type;
+   
+        
+    public News(String contenu, Journaliste auteur, boolean importance, TypeNews type){
+        this.contenu = contenu;
+        this.auteur = auteur;
+        this.importance = importance;
+        this.type = type;
+    }
+
+
 }
