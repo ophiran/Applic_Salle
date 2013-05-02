ef0097a43d49b1ed0ee6c3a2bf7847223342adf8
ophiran <ophiran@hotmail.com>
2013-05-02 09:55:31 +0200

    -

---
+++ src/libNews/StoringNewsBean.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libNews;
+
+import java.io.Serializable;
+import java.util.*;
+
+/**
+ *
+ * @author Ophiran
+ */
+public class StoringNewsBean{
+    public Vector<News> newsList = new Vector<News>();
+}
