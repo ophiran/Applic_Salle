cd2a1a42cdaf3ecfdefef57a968ab02fd9758800
ophiran <ophiran@hotmail.com>
2013-05-09 16:31:46 +0200

    -test

--- src/libNews/NewsCounterBean.java
+++ src/libNews/NewsCounterBean.java
@@ -38,7 +38,7 @@ public class NewsCounterBean implements PropertyChangeListener{
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        setNewsNumber((int)evt.getNewValue());
     }
     
     
