8db76d8971485b9b8e2edd0a9eb6088a8f5e160d
ekym <m.davidmike@gmail.com>
2013-05-09 16:05:26 +0200

    5

---
+++ src/process/ThreadNews.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package process;
+
+import network.NetworkStringReceiver;
+
+/**
+ *
+ * @author ekym
+ */
+public class ThreadNews extends Thread{
+    NetworkStringReceiver receiver;
+    
+    public void run(){
+        
+    }
+        
+        
+        
+}
