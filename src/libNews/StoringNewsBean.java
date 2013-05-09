2c0e09816ea1ef5354da539f00314e35f6acd487
ekym <m.davidmike@gmail.com>
2013-05-09 18:14:35 +0200

    a

--- src/libNews/StoringNewsBean.java
+++ src/libNews/StoringNewsBean.java
@@ -5,6 +5,7 @@
 package libNews;
 
 
+import java.beans.PropertyChangeSupport;
 import java.util.*;
 
 /**
@@ -13,6 +14,7 @@ import java.util.*;
  */
 public class StoringNewsBean implements StoreNewsListener{
     private Vector<News> newsList = new Vector<News>();
+    protected PropertyChangeSupport propertyHandling = new PropertyChangeSupport(this);
     
     public StoringNewsBean() {
         
--- src/process/ThreadNews.java
+++ src/process/ThreadNews.java
@@ -5,15 +5,33 @@
 package process;
 
 import network.NetworkStringReceiver;
+import libNews.NewsEvent;
+import java.util.*;
 
 /**
  *
  * @author ekym
  */
 public class ThreadNews extends Thread{
-    NetworkStringReceiver receiver;
+    protected NetworkStringReceiver receiver;
+    protected int listeningPort = 25678;
     
+    public ThreadNews(){
+        receiver = new NetworkStringReceiver(listeningPort);
+    }
     public void run(){
+        String receivedString;
+        String news;
+        String localisation;
+        do{
+            receivedString = receiver.getMessage();
+        }while(receivedString.equals("RIEN"));
+        StringTokenizer tok = new StringTokenizer(receivedString);
+        while (tok.hasMoreElements()){
+            
+        }
+        news.equals(receivedString.);
+        NewsEvent newsEvent = new newsEvent(this, "", "");
         
     }
         
